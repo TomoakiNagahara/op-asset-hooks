93e038fffb5c47b3c55b7baf72a065ab4c3749de
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-03-21 21:38:00 +0900

    Del: pre-rebase.php | GetRules() is move to GetRules.php.

--- pre-rebase.php
+++ pre-rebase.php
@@ -13,6 +13,11 @@
  */
 declare(strict_types=1);
 
+/**	Include
+ *
+ */
+require_once(__DIR__.'/GetRules.php');
+
 /**	Config file path.
  *
  *	<pre>
@@ -82,59 +87,6 @@ while( $commit = array_pop($commits) ){
 
 //	Success
 exit(0);
-
-/** Get rebase rules from php format config file.
- *
- * @created    2026-01-08
- * @return     array      $rules
- */
-function GetRules()
-{
-	//	Config file path.
-	$config_file = __DIR__.'/../../config/git-rebase-rules.php';
-
-	//	Load rule from php config file.
-	if( file_exists($config_file) ){
-		//	Load config value.
-		$rules = (function($config_file){
-			return include($config_file);
-		})($config_file);
-
-		//	Validate value.
-		if(!is_array($rules) ){
-			Fail("Config file must return array: $config_file");
-		}
-	}else{
-		$rules = [];
-	}
-
-	//	Check values.
-	foreach( ['prefix','deny'] as $key ){
-		//	...
-		if( isset($rules[$key]) ){
-		if(!is_array($rules[$key])){
-			Fail("The value of {$key} must be a array: type=".gettype($rules[$key]));
-		}
-		}
-
-		//	...
-		foreach( $rules[$key] ?? [] as $value ){
-			if (!is_string($value)) {
-				Fail("The value of {$key} must be a string.");
-			}
-		}
-	}
-
-	//	Add current branch name.
-	if( isset($rules['prefix']) ){
-		if( $branch = trim(`git branch --show-current` ?? '') ){
-			$rules['prefix'][] = "{$branch}: ";
-		}
-	}
-
-	//	Return rules.
-	return $rules;
-}
 
 /**	Get rebase target commit ids list.
  *
